b034a64c5f6b7c54748e3e2f2b3e029fb0314c8a
blumare85 <blumare85@gmail.com>
2024-10-21 16:37:35 +0900

    첫번째 작업

---
+++ demo.py
@@ -0,0 +1,9 @@
+# demo.py
+
+result = 3 + 5
+print(result)
+
+# 디버깅 셋팅
+for i in [1,2,3]:
+    print(i)
+
---
+++ ifelse데모.py
@@ -0,0 +1,26 @@
+# ifelse데모.py
+'''
+score = int(input("점수를 입력:"))
+
+if 90 <= score <= 100:
+    grade = "A : 토익 시험도 보세요"
+elif 80 <= score <= 90:
+    grade = "B : 열심히 일하세요"
+elif 70 <= score <= 80:
+    grade = "C : 희망을 가지세요"
+else:
+    grade = "F : 퇴사하세요"
+
+print("등급은 ", grade)
+'''
+
+# 반복구문
+value = 5
+while value > 0:
+    print(value)
+    value -= 1
+
+print("for ~ in 루프")
+lst = ["apple", 100, 3.14]
+for item in lst:
+    print(item)
